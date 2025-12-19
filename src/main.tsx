73f5f23b9a915bfab67ab92479c02c5c7e56d200
janeyeyey <jinpt0927@gmail.com>
2025-12-19 16:25:44 +0900

    Refactor main.tsx to include BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
-import { createRoot } from 'react-dom/client'
+import { createRoot } from "react-dom/client";
 import { ErrorBoundary } from "react-error-boundary";
-import "@github/spark/spark"
+import { BrowserRouter } from "react-router-dom";
 
-import App from './App.tsx'
-import { ErrorFallback } from './ErrorFallback.tsx'
+import App from "./App.tsx";
+import { ErrorFallback } from "./ErrorFallback.tsx";
 
-import "./main.css"
-import "./styles/theme.css"
-import "./index.css"
+import "./main.css";
+import "./styles/theme.css";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <ErrorBoundary FallbackComponent={ErrorFallback}>
-    <App />
-   </ErrorBoundary>
-)
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <App />
+    </BrowserRouter>
+  </ErrorBoundary>
+);
